12beac4da55f99957a6a9d83e4529dbf16173732
thomtt <truongthithom1999@gmail.com>
2021-10-21 16:58:20 +0700

    commit first

--- app/src/main/java/com/poly/lmsapp/ui/activity/RegisterActivity.java
+++ app/src/main/java/com/poly/lmsapp/ui/activity/RegisterActivity.java
@@ -8,6 +8,8 @@ import com.poly.lmsapp.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
