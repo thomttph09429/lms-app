0867ab1dc8e231e249c2b9d984e3c3c07d099661
Hoàng Đức Huy <69286440+huy6699@users.noreply.github.com>
2021-10-26 22:00:56 +0700

    register

--- app/src/main/java/com/poly/lmsapp/ui/activity/RegisterActivity.java
+++ app/src/main/java/com/poly/lmsapp/ui/activity/RegisterActivity.java
@@ -13,7 +13,7 @@ import android.widget.Toast;
 
 import com.poly.lmsapp.R;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
 
     TextView btnDangNhap;
     private EditText edtName, edtPassword, edtRePassWord, edtEmail;
@@ -23,55 +23,36 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        initViews();
+        initActions();
 
+
+    }
+
+    private void initViews() {
+        btnDangNhap.setOnClickListener(this);
+        btnDangKy.setOnClickListener(this);
+    }
+
+    private void initActions() {
         btnDangNhap = findViewById(R.id.btnDangNhap);
         edtName = findViewById(R.id.edtName);
         edtEmail = findViewById(R.id.edtEmail);
         edtPassword = findViewById(R.id.edtPassword);
         edtRePassWord = findViewById(R.id.edtRePassword);
-
         btnDangKy = findViewById(R.id.btnDangKy);
-        btnDangKy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                checkCredentials();
-            }
-        });
-
-        btnDangNhap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
-            }
-        });
     }
 
-    private void checkCredentials() {
-        String name=edtName.getText().toString();
-        String email=edtEmail.getText().toString();
-        String password=edtPassword.getText().toString();
-        String repassword=edtRePassWord.getText().toString();
 
-        if (name.isEmpty() || name.length()<3)
-        {
-            showError(edtName, "Tên của bạn không hợp lệ!");
-        }else if (email.isEmpty() || !email.contains("@"))
-        {
-            showError(edtEmail, "Email của bạn không hợp lệ");
-        }else if (password.isEmpty() || password.length()<7)
-        {
-            showError(edtPassword, "Mật khẩu của bạn phải từ 8 ký tự trở lên");
-        }else if (repassword.isEmpty() || !repassword.equals(password))
-        {
-            showError(edtRePassWord, "Mật khẩu không trùng khớp");
-        }else
-        {
-            Toast.makeText(this, "Đăng ký thành công", Toast.LENGTH_LONG).show();
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnDangKy:
+                startActivity(new Intent(this,LoginActivity.class));
+                break;
+            case R.id.btnDangNhap:
+
+                break;
         }
-    }
-
-    private void showError(EditText edt, String s) {
-        edt.setError(s);
-        edt.requestFocus();
     }
 }
