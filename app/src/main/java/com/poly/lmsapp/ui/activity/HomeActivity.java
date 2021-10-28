5389d6ce0b99c4a75fd88e38d6a04030c004c38d
Ninh Vi <71386459+ninhvvbg@users.noreply.github.com>
2021-10-28 23:19:42 +0700

    update homeactivity

--- app/src/main/java/com/poly/lmsapp/ui/activity/HomeActivity.java
+++ app/src/main/java/com/poly/lmsapp/ui/activity/HomeActivity.java
@@ -1,8 +1,13 @@
 package com.poly.lmsapp.ui.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.poly.lmsapp.R;
 
@@ -12,5 +17,23 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_setings,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(this,"Item 1 click",Toast.LENGTH_SHORT).show();
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
