d5b6dc353f75e8f4f4d62e5327526933ed291d76
Ninh Vi <71386459+ninhvvbg@users.noreply.github.com>
2021-11-04 19:43:18 +0700

    monhoc kho hoc lieu update

---
+++ app/src/main/java/com/poly/lmsapp/ui/khohoclieu/MH_Khohoclieu_Activity.java
@@ -0,0 +1,16 @@
+package com.poly.lmsapp.ui.khohoclieu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.poly.lmsapp.R;
+
+public class MH_Khohoclieu_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mh_khohoclieu);
+    }
+}
